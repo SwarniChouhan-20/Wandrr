adcdfba7e597a57d5b4a39355e3ae706492f52ed
SwarniChouhan-20 <swarni.c@somaiya.edu>
2026-04-02 23:27:58 +0530

    minor fixes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/home";
 import PlanTrip from "./components/PlanTrip";
@@ -7,7 +7,18 @@ import PlanSummary from "./pages/PlanSummary";
 import Login from "./components/login";
 import Signup from "./components/Signup";
 import AboutUs from "./components/about";
-import Feedback from "./components/Feedback"
+import Feedback from "./components/Feedback";
+
+const isAuthenticated = () => {
+  return Boolean(localStorage.getItem("token"));
+};
+
+const PrivateRoute = ({ children }) => {
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
 export default function App() {
   return (
@@ -16,13 +27,27 @@ export default function App() {
       <div style={{ paddingTop: "60px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/plan-trip" element={<PlanTrip />} />
-          <Route path="/plan-summary" element={<PlanSummary />} />
-          <Route path="*" element={<Home />} />  {/* default redirect */}
+          <Route
+            path="/plan-trip"
+            element={
+              <PrivateRoute>
+                <PlanTrip />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/plan-summary"
+            element={
+              <PrivateRoute>
+                <PlanSummary />
+              </PrivateRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/feedback" element={<Feedback />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
